246a210541968deae4eca17bb335453b79794030
Lizurt <lizard.x@mail.ru>
2022-05-25 15:57:50 +0300

    added an ability to view all metrics

--- src/main/java/com/muzkat/server/controller/MetricController.java
+++ src/main/java/com/muzkat/server/controller/MetricController.java
@@ -1,5 +1,6 @@
 package com.muzkat.server.controller;
 
+import com.muzkat.server.model.entity.MetricEntity;
 import com.muzkat.server.model.request.CountInMetricRequest;
 import com.muzkat.server.model.request.IncreaseMetricRequest;
 import com.muzkat.server.repository.MetricRepository;
@@ -8,11 +9,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Tag(name = "Metric controller", description = "Controls all metric-related HTTP requests.")
 @RestController
 public class MetricController {
     @Autowired
     private MetricService metricService;
+    @Autowired
+    private MetricRepository metricRepository;
 
     @PostMapping("/metric/count")
     public void tryCountInMetric(@RequestBody CountInMetricRequest countInMetricRequest) {
@@ -22,6 +27,11 @@ public class MetricController {
     @GetMapping("/metric/count/{name}")
     public Integer getCount(@PathVariable String name) {
         return metricService.getCount(name);
+    }
+
+    @GetMapping("/metric/get-all")
+    public List<MetricEntity> getAll() {
+        return metricRepository.findAll();
     }
 
     @PutMapping("/metric/inc")
